4977686ea46e4b45f0756c28dd6988eb027d286a
ErwanHermawan <erwanhermawan858@gmail.com>
2021-09-18 09:59:08 +0700

    init

---
+++ src/scripts/components/index.js
@@ -0,0 +1,14 @@
+/* ------------------------------------------------------------------------------
+@name: Components Index
+@description: Indexing all components
+--------------------------------------------------------------------------------- */
+
+import WinScroll from "./WinScroll";
+import WindowResize from "./WindowResize";
+import Header from "./Header";
+
+export {
+  WinScroll,
+  WindowResize,
+  Header
+};
